0fca1c754cf369d4bbd5973383ed9dbff4452c39
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 01:40:19 -0300

    entidades

---
+++ DocumentBase.cs
@@ -0,0 +1,18 @@
+﻿using Cosmonaut.Attributes;
+using trifenix.connect.model;
+
+namespace trifenix.connect.entities.cosmos
+{
+    public abstract class DocumentBase : DocumentDb {
+
+
+        [CosmosPartitionKey]
+        public string CosmosEntityName { get; set; }
+
+        protected DocumentBase() {
+            CosmosEntityName = GetType().Name;
+        }
+
+    }
+
+}
---
+++ DocumentBaseName.cs
@@ -0,0 +1,9 @@
+﻿namespace trifenix.connect.entities.cosmos
+{
+    public abstract class DocumentBaseName : DocumentBase {
+
+        public abstract string Name { get; set; }
+
+    }
+
+}
