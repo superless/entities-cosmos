28d48066d84dfd0f8a79d3e124de207a791d984e
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 02:18:04 -0300

    agregando documentación

--- DocumentBase.cs
+++ DocumentBase.cs
@@ -3,12 +3,23 @@ using trifenix.connect.model;
 
 namespace trifenix.connect.entities.cosmos
 {
+    /// <summary>
+    /// Entidad base para modelos de bases de datos basados en cosmonaut.
+    /// </summary>
     public abstract class DocumentBase : DocumentDb {
 
 
+        /// <summary>
+        /// Partición dentro de base de datos documental,
+        /// esto permite a través de un indice particionar un segmento del indice de la base de datos
+        /// por nombre de la entidad o nombre de la clase.
+        /// </summary>        
         [CosmosPartitionKey]
         public string CosmosEntityName { get; set; }
 
+        /// <summary>
+        /// Asigna el nombre de la clase como partición.
+        /// </summary>
         protected DocumentBase() {
             CosmosEntityName = GetType().Name;
         }
--- DocumentBaseName.cs
+++ DocumentBaseName.cs
@@ -1,7 +1,13 @@
 ﻿namespace trifenix.connect.entities.cosmos
 {
+    /// <summary>
+    /// Hereda de documentBase, para agregar un nombre al elemento.
+    /// </summary>
     public abstract class DocumentBaseName : DocumentBase {
-
+        
+        /// <summary>
+        /// Nombre del elemento
+        /// </summary>        
         public abstract string Name { get; set; }
 
     }
